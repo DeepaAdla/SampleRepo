1a20340e37b007a9fa4c90d9f065f773d825be7e
BPSH023 <BPSH023@192.168.1.56>
2017-09-20 15:26:51 +0530

    Initial Commit

---
+++ SampleApp23/WebContent/sampleapp23/SampleApp.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("sampleapp23.SampleApp", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf sampleapp23.SampleApp
+	*/ 
+	getControllerName : function() {
+		return "sampleapp23.SampleApp";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf sampleapp23.SampleApp
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
